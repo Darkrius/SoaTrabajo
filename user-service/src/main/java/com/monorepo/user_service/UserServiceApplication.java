4ad4fd0960faebc74a7a8c7b71a5616fe8e5a226
Darkrius <Piscontesoto@gmail.com>
2025-08-26 20:02:57 -0500

    subiendo estructura

---
+++ user-service/src/main/java/com/monorepo/user_service/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.monorepo.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
